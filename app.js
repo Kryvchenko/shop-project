74969f220fd962c4a2f4a2ff6a736a32faad2fac
Тигр <amzkryvchenko@gmail.com>
2022-01-11 22:00:13 -0400

    js code updated

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-console.log("text");
+//console.log("text");
 const productsCountEl = document.getElementById("products-count");
 console.log(productsCountEl);
 
-const addToCartsBtns = document.querySelectorAll(".btn-cart");
-console.log(addToCartsBtns);
+const addToCartBtns = document.querySelectorAll(".btn-cart");
+console.log(addToCartBtns);
 
 
-for (let i = 0; i < addToCartsBtns.length; i++) {
-    addToCartsBtns[i].addEventListener("click", function () {
-      console.log("clicked");
+for (let i = 0; i < addToCartBtns.length; i++) {
+    addToCartBtns[i].addEventListener("click", function () {
+      productsCountEl.textContent = `${event.detail}`;
     });
   };
 
+
+ const productsLike = document.getElementById("heart");
+ console.log(productsLike);
+
+ const heartLikeBtns = document.querySelectorAll(".like.small-btn");
+ console.log(heartLikeBtns);
+ 
+ 
