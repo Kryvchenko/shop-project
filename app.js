dc8e650d7c5b6497010dd251bb4a0c8ab5b89cd4
Тигр <amzkryvchenko@gmail.com>
2022-01-12 14:57:25 -0400

    js code added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//console.log("text");
+
 const productsCountEl = document.getElementById("products-count");
 console.log(productsCountEl);
 
@@ -8,19 +8,73 @@ console.log(addToCartBtns);
 
 for (let i = 0; i < addToCartBtns.length; i++) {
     addToCartBtns[i].addEventListener("click", function () {
-      productsCountEl.textContent = `${event.detail}`;
+      productsCountEl.textContent = +productsCountEl.textContent + 1;
     });
   };
 
 
- const productsLike = document.getElementById("heart");
- console.log(productsLike);
-
- const heartLikeBtns = document.querySelectorAll(".like.small-btn");
- console.log(heartLikeBtns);
- 
- heartLikeBtns.addEventListener("click", function () {
-  console.log("clicked");
-  document.getElementById("heart").style.backgroundColor = "#2c71b8";
-
+// change like state
+const likeBtns = document.querySelectorAll(".like");
+likeBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("liked");
+  });
 });
+
+
+//modal 
+const modal = document.querySelector(".modal");
+const moreDetailsBtns = document.querySelectorAll(".btn-details");
+const btnClose = document.querySelector(".btn-close");
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+moreDetailsBtns.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+
+btnClose.addEventListener("click", closeModal);
+
+
+
+
+
+
+
+/*const detailsBtns = document.querySelectorAll(".btn-details");
+console.log(detailsBtns);
+detailsBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("show");
+  });
+});*/
+
+//let prevProductCount = +productsCountEl.textContent;
+//productsCountEl.textContent = prevProductCount + 1;
+
+/*const likeBtns = document.querySelectorAll(".like");
+likeBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    if(item.classList.contains("liked")) {
+      item.classList.remove("liked"); 
+    } else {
+      item.classList.add("liked"); 
+    }
+    console.log("liked");
+  });
+});*/
+
+
+
+
+
+
+
+
